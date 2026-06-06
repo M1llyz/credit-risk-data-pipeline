463db144f43fa417dbb4b867afba94c34f18d9e6
M1llyz <005jamilly@gmail.com>
2026-06-06 19:19:48 -0300

    fix: update project structure and pipeline orchestration

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from src.ingestion.download_data import main as download_main
 from src.processing.transform_data import main as transform_main
+from src.processing.generate_gold import main as gold_main
 from src.loading.load_to_sqlserver import main as load_main
 
 def main():
     download_main()
     transform_main()
+    gold_main()
     load_main()
 
 if __name__ == "__main__":
     main()
-    
